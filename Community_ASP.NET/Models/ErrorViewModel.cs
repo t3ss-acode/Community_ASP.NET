7a96f43bc9e5e250c2a92f11d6295e8f664e0536
Rasmus Jansson <rasmusja@UG.KTH.SE>
2020-10-12 15:09:28 +0200

    Initial commit

---
+++ Community_ASP.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Community_ASP.NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
