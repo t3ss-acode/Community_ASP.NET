e8b96bfe930123d36e9a89febf5e50945654da24
t3ss <theank@kth.se>
2020-10-15 09:58:55 +0200

    Identity added

---
+++ Community_ASP.NET/Areas/Identity/Data/Community_ASPNETUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Community_ASP.NET.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the Community_ASPNETUser class
+    public class Community_ASPNETUser : IdentityUser
+    {
+    }
+}
---
+++ Community_ASP.NET/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Community_ASP.NET.Areas.Identity.Data;
+using Community_ASP.NET.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Community_ASP.NET.Areas.Identity.IdentityHostingStartup))]
+namespace Community_ASP.NET.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Community_ASPNETContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("Community_ASPNETContextConnection")));
+
+                services.AddDefaultIdentity<Community_ASPNETUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<Community_ASPNETContext>();
+            });
+        }
+    }
+}
