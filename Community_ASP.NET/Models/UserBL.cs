3476aa915e6f0565d84aee8cfa69cda0d1b9b2aa
theank <theank@kth.se>
2020-11-27 11:59:25 +0100

    Manual update. Last changes for E

--- Community_ASP.NET/Models/UserBL.cs
+++ Community_ASP.NET/Models/UserBL.cs
@@ -14,9 +14,27 @@ namespace Community_ASP.NET.Models
             UserDAL.AddUserToDB(user);
         }
 
-        public static IEnumerable<Community_ASPNETUser> GetUsers()
+        public static IEnumerable<UserInfo> GetUsers()
         {
-            return UserDAL.GetUsers();
+            var userList = UserDAL.GetUsers();
+            var userInfoList = new List<UserInfo>();
+
+            foreach(Community_ASPNETUser user in userList) 
+            {
+                var userInfo = new UserInfo();
+                userInfo.Name = user.name;
+                userInfo.Email = user.Email;
+                userInfo.LastLogin = latestLogin(user);
+                userInfo.NrOfLoginsLastMonth = numberOfLogins(user);
+                userInfo.TotalMessages = numberOfMsg(user);
+                userInfo.NrOfUnreadMessages = numberOfUnreadMsg(user);
+                userInfo.NrOfDeletedMessages = user.numberOfDeletedMessages;
+
+                userInfoList.Add(userInfo);
+            }
+
+            
+            return userInfoList;
         }
 
         public static UserInfo GetUser(string userId)
@@ -36,6 +54,34 @@ namespace Community_ASP.NET.Models
 
             return userInfo;
         }
+
+        public static UserInfo GetUserWithEmail(string userEmail)
+        {
+            Community_ASPNETUser user = UserDAL.GetUserWithEmail(userEmail);
+
+            var userInfo = new UserInfo();
+            userInfo.Name = user.name;
+            userInfo.Email = user.Email;
+            userInfo.LastLogin = latestLogin(user);
+            userInfo.NrOfLoginsLastMonth = numberOfLogins(user);
+            userInfo.TotalMessages = numberOfMsg(user);
+            userInfo.NrOfUnreadMessages = numberOfUnreadMsg(user);
+            userInfo.NrOfDeletedMessages = user.numberOfDeletedMessages;
+
+            userInfo.NrOfReadMessages = userInfo.TotalMessages - userInfo.NrOfUnreadMessages;
+
+            return userInfo;
+        }
+
+
+
+        public static String GetUserIdString(String email)
+        {
+            Community_ASPNETUser user = UserDAL.GetUserWithEmail(email);
+
+            return user.Id;
+        }
+
 
         public static void UpdateUser(Community_ASPNETUser user)
         {
